60c4ac460f770e7268bfe988a5f301d1d17e6270
Trung DG <thanhtrung.it25@gmail.com>
2021-03-05 14:08:29 +0700

    Normalize state, remove eslint, app-state interface

--- src/interfaces/diary.interface.ts
+++ src/interfaces/diary.interface.ts
@@ -1,9 +1,6 @@
-import { Entry } from './entry.interface';
-
 export interface Diary {
   id: number;
   title: string;
-  entries: Entry[];
   type: 'private' | 'public';
   createdAt: string;
   updatedAt: string;
