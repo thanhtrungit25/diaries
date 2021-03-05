8760a910c712d43646907907b2bc1c7033873987
Trung DG <thanhtrung.it25@gmail.com>
2021-03-05 15:29:24 +0700

    Add user slice

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -3,12 +3,14 @@ import appReducer from './appSlice';
 import authReducer from '../features/auth/authSlice';
 import diariesReducer from '../features/diary/diariesSlice';
 import entriesReducer from '../features/entry/entriesSlice';
+import userReducer from '../features/auth/userSlice';
 
 const rootReducer = combineReducers({
   app: appReducer,
   auth: authReducer,
   diaries: diariesReducer,
   entries: entriesReducer,
+  user: userReducer,
 });
 
 export type RootState = typeof rootReducer;
