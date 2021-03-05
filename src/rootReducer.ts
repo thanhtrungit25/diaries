88147b7915fd5fb8bc8cceccd25c4802def30747
Trung DG <thanhtrung.it25@gmail.com>
2021-03-05 15:43:32 +0700

    Remove appSlice

--- src/rootReducer.ts
+++ src/rootReducer.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import appReducer from './app/appSlice';
 import authReducer from './features/auth/authSlice';
 import diariesReducer from './features/diary/diariesSlice';
 import entriesReducer from './features/entry/entriesSlice';
 import userReducer from './features/auth/userSlice';
 
 const rootReducer = combineReducers({
-  app: appReducer,
   auth: authReducer,
   diaries: diariesReducer,
   entries: entriesReducer,
