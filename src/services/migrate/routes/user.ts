cdc0a1d3ddc42001dbc8600bb94f15ef1ba16cd8
Trung DG <thanhtrung.it25@gmail.com>
2021-03-05 16:21:00 +0700

    Update user route handler with token use AuthResonse interface

--- src/services/migrate/routes/user.ts
+++ src/services/migrate/routes/user.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from 'miragejs';
+import { randomBytes } from 'crypto';
 import { User } from '../../../interfaces/user.interface';
 import { handleErrors } from '../server';
 
-const login = (schema: any, req: Request): User | Response => {
+const generateToken = () => randomBytes(8).toString('hex');
+
+export interface AuthResponse {
+  token: string;
+  user: User;
+}
+
+const login = (schema: any, req: Request): AuthResponse | Response => {
   const { username, password } = JSON.parse(req.requestBody);
   const user = schema.users.findBy({ username });
   if (!user) {
@@ -11,16 +19,25 @@ const login = (schema: any, req: Request): User | Response => {
   if (password !== user.password) {
     return handleErrors(null, 'Password is incorrect');
   }
-  return user.attrs as User;
+
+  const token = generateToken();
+  return {
+    user: user.attrs as User,
+    token,
+  };
 };
-const signup = (schema: any, req: Request) => {
+const signup = (schema: any, req: Request): AuthResponse | Response => {
   const data = JSON.parse(req.requestBody);
   const exUser = schema.users.findBy({ username: data.username });
   if (exUser) {
     return handleErrors(null, 'A user with that username already exists.');
   }
   const user = schema.users.create(data);
-  return user.attrs as User;
+  const token = generateToken();
+  return {
+    user: user.attrs as User,
+    token,
+  };
 };
 
 export default {
