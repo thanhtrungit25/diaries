d3935bda8cd4100f053b6851442e44370b572fad
Trung DG <thanhtrung.it25@gmail.com>
2021-03-07 13:15:12 +0700

    Update local edited entry when - have currentEditing entry changed - setCanEdit to false when create/update entry - add conditon render markdown component after render add or update

--- src/features/entry/Editor.tsx
+++ src/features/entry/Editor.tsx
@@ -1,11 +1,12 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import Swal from 'sweetalert2';
 import { useSelector, useDispatch } from 'react-redux';
+import Markdown from 'markdown-to-jsx';
 import { AppDispatch } from '../../store';
 import http from '../../services/api';
 import { Diary } from '../../interfaces/diary.interface';
 import { Entry } from '../../interfaces/entry.interface';
-import { setCurrentlyEditing } from './editorSlice';
+import { setCurrentlyEditing, setCanEdit } from './editorSlice';
 import { updateDiary } from '../diary/diariesSlice';
 import { updateEntry } from './entriesSlice';
 
@@ -16,6 +17,10 @@ const Editor: FC = () => {
 
   const [editedEntry, updateEditedEntry] = useState(entry);
   const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    updateEditedEntry(entry);
+  }, [entry]);
 
   const saveEntry = async () => {
     if (activeDiaryId === null) {
@@ -51,55 +56,83 @@ const Editor: FC = () => {
         });
     }
 
-    updateEditedEntry({
-      ...editedEntry,
-      title: '',
-      content: '',
-    });
+    dispatch(setCanEdit(false));
   };
 
   return (
     <div className="editor">
-      <header>
-        <input
-          value={editedEntry?.title ?? ''}
-          disabled={!canEdit}
-          onChange={(e) => {
-            if (editedEntry) {
-              updateEditedEntry({
-                ...editedEntry,
-                title: e.target.value,
-              });
-            } else {
-              updateEditedEntry({
-                title: e.target.value,
-                content: '',
-              });
-            }
-          }}
-        />
-      </header>
-      <textarea
-        placeholder="Supports markdonw!"
-        disabled={!canEdit}
-        value={editedEntry?.content ?? ''}
-        onChange={(e) => {
-          if (editedEntry) {
-            updateEditedEntry({
-              ...editedEntry,
-              content: e.target.value,
-            });
-          } else {
-            updateEditedEntry({
-              title: '',
-              content: e.target.value,
-            });
-          }
+      <header
+        style={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+          marginBottom: '0.2em',
+          paddingBottom: '0.2em',
+          borderBottom: '1px solid rgba(0,0,0,0.1)',
         }}
-      />
-      <button onClick={saveEntry} disabled={!canEdit}>
-        Save
-      </button>
+      >
+        {entry && !canEdit ? (
+          <h4>
+            {entry.title}
+            <a
+              href="#edit"
+              onClick={(e) => {
+                e.preventDefault();
+                if (entry != null) {
+                  dispatch(setCanEdit(true));
+                }
+              }}
+            >
+              (Edit)
+            </a>
+          </h4>
+        ) : (
+          <input
+            value={editedEntry?.title ?? ''}
+            disabled={!canEdit}
+            onChange={(e) => {
+              if (editedEntry) {
+                updateEditedEntry({
+                  ...editedEntry,
+                  title: e.target.value,
+                });
+              } else {
+                updateEditedEntry({
+                  title: e.target.value,
+                  content: '',
+                });
+              }
+            }}
+          />
+        )}
+      </header>
+      {entry && !canEdit ? (
+        <Markdown>{entry.content}</Markdown>
+      ) : (
+        <>
+          <textarea
+            placeholder="Supports markdonw!"
+            disabled={!canEdit}
+            value={editedEntry?.content ?? ''}
+            onChange={(e) => {
+              if (editedEntry) {
+                updateEditedEntry({
+                  ...editedEntry,
+                  content: e.target.value,
+                });
+              } else {
+                updateEditedEntry({
+                  title: '',
+                  content: e.target.value,
+                });
+              }
+            }}
+          />
+          <button onClick={saveEntry} disabled={!canEdit}>
+            Save
+          </button>
+        </>
+      )}
     </div>
   );
 };
